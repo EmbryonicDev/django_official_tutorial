4e2382c52540c9fb2603ec859cffcadcdc5a8f17
djdevlam-apple <embryonic.dev@gmail.com>
2023-04-07 17:27:55 +0200

    Add __str__ to models: Question & Choice

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -9,8 +9,14 @@ class Question(models.Model):
     # pub_date = machine-readable
     # 'date published' = human-readable
 
+    def __str__(self):
+        return self.question_text
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
