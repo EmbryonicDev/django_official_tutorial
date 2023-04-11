d9fe0b83af6b9091dad9dbdfc83362e223fd12a2
djdevlam-apple <embryonic.dev@gmail.com>
2023-04-11 12:12:40 +0200

    Modify polls/views / def index to use render instead of HttpResponse

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
+from django.template import loader
 
 
 def index(request):
@@ -8,9 +9,18 @@ def index(request):
     # return HttpResponse(("Hello, world. You're at the polls index"))
 
     # Part 3
+    # slower method
+    # latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    # template = loader.get_template("polls/index.html")
+    # context = {
+    #     "latest_question_list": latest_question_list
+    # }
+    # return HttpResponse(template.render(context, request))
+
+    # shortcut version
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {"latest_question_list": latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
