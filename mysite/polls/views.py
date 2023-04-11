ca0109df1e8a60d85f2b594ed546899234747a55
djdevlam-apple <embryonic.dev@gmail.com>
2023-04-11 12:29:27 +0200

    Refactor polls/views.py / def detail to use less lines of code

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from .models import Question
 from django.template import loader
 
@@ -24,7 +24,16 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    # Part 3, method 1
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404('Question does not exist')
+    # return render(request, 'polls/detail.html', {'question': question})
+
+    # shortcut version
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
