4ad5f2a5cd49702997be2f8e9a3cf23043ab684d
djdevlam-apple <embryonic.dev@gmail.com>
2023-04-12 14:49:27 +0200

    Update QuestionAdmin fieldset to use fieldset format

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,7 +3,15 @@ from .models import Question, Choice
 
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ["pub_date", "question_text"]
+    # Fields without using fieldset format
+    # fields = ["pub_date", "question_text"]
+
+    # Using fieldset format.
+    # 1st field in tuple is title
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
 
 
 admin.site.register(Question, QuestionAdmin)
