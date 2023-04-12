e500b87d670538707c755ae5394561be9355e19b
djdevlam-apple <embryonic.dev@gmail.com>
2023-04-12 16:23:52 +0200

    Change ChoiceInline view to tabular view
    Change ChoiceInline(admin.StackedInline) -> (admin.TabularInLine)

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from .models import Question, Choice
 
+# Stacked view that is not efficient with space
+# in this instance:
+# class ChoiceInline(admin.StackedInline):
 
-class ChoiceInline(admin.StackedInline):
+# Tabular view
+
+
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
@@ -10,7 +16,8 @@ class ChoiceInline(admin.StackedInline):
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {"fields": ["question_text"]}),
-        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+        ("Date information", {"fields": [
+         "pub_date"], "classes": ["collapse"]}),
     ]
     inlines = [ChoiceInline]
 
